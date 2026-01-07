d3df2779c3d78f61412cfba2f190141da370e308
LenX1 <lennart.chario@outlook.de>
2026-01-07 10:52:06 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$username = "";
+$password = "";
+$dbname = "DBMitarbeiter";
+$tablename = "TableMitarbeiter";
+$tableheader = "ID INT(255) PRIMARY KEY, Vorname VARCHAR(255), Name VARCHAR(255), Abteilung VARCHAR(255), Gehalt INT(255), Eintrittsdatum VARCHAR(255), Status VARCHAR(255)";
+$csvname = "daten.csv";
+
+?>
