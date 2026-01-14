38ee3d9bff8d1ebadca6650cadb9e2a73f3247b7
LenX1 <234128646+LenX1@users.noreply.github.com>
2026-01-14 09:27:29 +0100

    Update insertTable.php

--- insertTable.php
+++ insertTable.php
@@ -5,13 +5,12 @@
             $connection = new PDO("mysql:host=$host;dbname=$dbName", $userName, $password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	    if(!is_numeric($gehalt)) {$gehalt=0;}
-	    if($eintrittsdatum == '') {$eintrittsdatum = date("Y-m-d");}
+	    	if(!is_numeric($gehalt)) {$gehalt=0;}
+	    	if($eintrittsdatum == '') {$eintrittsdatum = date("Y-m-d");}
 
-	    $statement = $connection->prepare("INSERT INTO $tableName (ID, Vorname, Name, Abteilung, Gehalt, Eintrittsdatum, Status) VALUES ($id, \"$firstName\", \"$lastName\", \"$department\", $salery, \"$hireDate\", \"$status\")");
+	    	$statement = $connection->prepare("INSERT INTO $tableName (ID, Vorname, Name, Abteilung, Gehalt, Eintrittsdatum, Status) VALUES ($id, \"$firstName\", \"$lastName\", \"$department\", $salery, \"$hireDate\", \"$status\")");
             $statement->execute();
 
-            //echo "New records created successfully";
         } catch(PDOException $error) {
             echo "InsertTable - Error: " . $error->getMessage();
         }
